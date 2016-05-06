35237c19a9e3f914e93850b2dc0b8d8ecfff1255
alu0100505009 <alu0100505009@ull.edu.es>
2016-05-06 09:27:21 +0000

    Añadido get para mostrar los botones

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 "use strict";
 
-
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
+const datos = require('./models/conexiones.js');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -28,4 +27,14 @@ app.get('/csv', (request, response) => {
 
 app.listen(app.get('port'), () => {
     console.log(`Node app is running at localhost: ${app.get('port')}` );
+});
+
+
+app.get('/mostrarBotones', function(req, res) {
+    datos.find({}, function(err, file) {
+        if (err)
+            return err;
+        console.log("file: " + file);
+        res.send(file);
+    });
 });
